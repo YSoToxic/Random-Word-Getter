de0de73aa86ed7ba121ea60a6aad597b8d693ec7
Dom <developer@ysotoxic.com>
2021-07-30 22:21:00 +1000

    The initial commit of the windows part of this project

---
+++ Desktop/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Desktop
+{
+    public partial class Form1 : Form
+    {
+        List<string> lines = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string filePath = @"" + textBox1.Text;
+            lines = File.ReadAllLines(filePath).ToList();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int value = random.Next(0, lines.Count);
+            label3.Text = lines[value];
+        }
+    }
+}
